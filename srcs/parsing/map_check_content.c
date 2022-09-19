72ff0f378c080c1c0fa49629216e210498b85fdd
Adrian WEAVER <aweaver@student.42.fr>
2022-09-19 09:42:16 +0200

    WIP MAP PARSING:
    Adding first test cases checking floor and ceilling lines matching
    patterns.
    
    This is a WIP

--- srcs/parsing/map_check_content.c
+++ srcs/parsing/map_check_content.c
@@ -6,14 +6,38 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/16 13:06:38 by aweaver           #+#    #+#             */
-/*   Updated: 2022/09/19 09:48:58 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/09/19 09:51:36 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	ft_check_map_content(t_cub_data *data)
+int	ft_check_floor(t_cub_data *data, char *line)
 {
 	(void)data;
+	printf("floor line = %s\n", line);
+	return (0);
+}
+
+int	ft_check_ceilling(t_cub_data *data, char *line)
+{
+	(void)data;
+	printf("ceilling line = %s\n", line);
+	return (0);
+}
+
+int	ft_check_map_content(t_cub_data *data)
+{
+	t_map_data	*map;
+
+	while (data->lst_map)
+	{
+		map = (t_map_data *)data->lst_map;
+		if (ft_strncmp(map->line, "F", 1) == 0)
+			ft_check_floor(data, map->line);
+		else if (ft_strncmp(map->line, "C", 1) == 0)
+			ft_check_ceilling(data, map->line);
+		data->lst_map = data->lst_map->next;
+	}
 	return (0);
 }

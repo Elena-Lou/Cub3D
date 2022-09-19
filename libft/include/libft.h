81d8a1f378e2c189489f518f06875c178ad9e0cd
Adrian WEAVER <aweaver@student.42.fr>
2022-09-19 11:55:18 +0200

    ADD FT_SPLIT_F FUNCTION:
    Adding a split function to use with a function instead of an int.
    Check prototype to know how to use it.
    
    Changes to be committed:
    	modified:   Makefile
    	new file:   ft_split_f.c
    	modified:   include/libft.h

--- libft/include/libft.h
+++ libft/include/libft.h
@@ -62,6 +62,7 @@ char	*ft_strdup(const char *s);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
+char	**ft_split_f(char const *s, int (*funct)(int arg));
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 void	ft_striteri(char *s, void (*f)(unsigned int, char*));

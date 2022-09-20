c016254868fbacef4db81b0eca087c982a97c087
Adrian WEAVER <aweaver@student.42.fr>
2022-09-20 12:11:44 +0200

    FIX WRONG ERROR MESSAGE:
    Fixing wrong error message for incorrect map.
    Needs to be changed for the other error messages.

--- srcs/parsing/check_map_name.c
+++ srcs/parsing/check_map_name.c
@@ -17,11 +17,11 @@ int	ft_check_name(char *map_name)
 	size_t	map_name_size;
 
 	if (map_name == NULL)
-		return (write(2, "Wrong map name\n", 15), 1);
+		return (write(2, "Error\nWrong map name\n", 21), 1);
 	map_name_size = ft_strlen(map_name);
 	if (map_name_size < 4)
-		return (write(2, "Wrong map name\n", 15), 1);
+		return (write(2, "Error\nWrong map name\n", 21), 1);
 	if (ft_strncmp(&map_name[map_name_size - 4], ".cub", 4) != 0)
-		return (write(2, "Wrong map name\n", 15), 1);
+		return (write(2, "Error\nWrong map name\n", 21), 1);
 	return (0);
 }

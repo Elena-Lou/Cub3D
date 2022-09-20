ce35ffed2c83e6a26a6bb953548c11116612234a
Adrian WEAVER <aweaver@student.42.fr>
2022-09-20 12:12:31 +0200

    FIX FT_FREE_DOUBLE_ARRAY:
    Fixing incorrect read in case of wrong pointer given.
    Now secured.

--- libft/ft_free_double_array.c
+++ libft/ft_free_double_array.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/20 08:28:14 by aweaver           #+#    #+#             */
-/*   Updated: 2022/09/20 08:28:33 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/09/20 12:01:15 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ void	ft_free_double_array(char **str)
 	int		i;
 
 	i = 0;
+	if (str == NULL)
+		return ;
 	while (str[i])
 	{
 		free(str[i]);

9b4b60bdaf5cf78479efd0eebf4ff3475d318494
Elena LOUISIADE <elouisia@e1r5p2.clusters.42paris.fr>
2022-09-21 14:09:23 +0200

    FIX FUNCTIONS NAMES:
    For consistency and to better discriminate between built-in
    functions and our owns, "ft" is added at the start of the functions names
    - ft_add_line_map
    - ft_map_to_list
    - ft_close_window
    - ft_key_check

--- srcs/main.c
+++ srcs/main.c
@@ -6,22 +6,22 @@
 /*   By: elouisia <elouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/06 15:52:30 by elouisia          #+#    #+#             */
-/*   Updated: 2022/09/26 10:22:44 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/09/28 15:41:27 by elouisia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	key_check(int key, t_cub_data *data)
+int	ft_key_check(int key, t_cub_data *data)
 {
 	if (key == XK_Escape)
-		close_window(data);
+		ft_close_window(data);
 	else
 		printf("%d\n", key);
 	return (0);
 }
 
-int	close_window(t_cub_data *data)
+int	ft_close_window(t_cub_data *data)
 {
 	mlx_destroy_window(data->mlx_ptr, data->win_ptr);
 	mlx_destroy_display(data->mlx_ptr);
@@ -54,17 +54,17 @@ int	main(int ac, char **av)
 		return (1);
 	}
 	ft_init_data(&data);
-	if (map_to_list(&data, av[1]))
-		return (0);
-	ft_check_map_content(&data);
+	ft_map_to_list(&data, av[1]);
+	if (ft_check_map_content(&data) == 1)
+		return (ft_lstclear(&(data.lst_map), ft_clear_map), 1);
 	data.mlx_ptr = mlx_init();
 	if (!data.mlx_ptr)
 		return (ft_lstclear(&(data.lst_map), ft_clear_map), 1);
 	data.win_ptr = mlx_new_window(data.mlx_ptr, 1000, 800, "Cub3D");
 	if (!data.win_ptr)
 		return (ft_lstclear(&(data.lst_map), ft_clear_map), 1);
-	mlx_key_hook(data.win_ptr, &key_check, &data);
-	mlx_hook(data.win_ptr, 17, 1L << 3, &close_window, &data);
+	mlx_key_hook(data.win_ptr, &ft_key_check, &data);
+	mlx_hook(data.win_ptr, 17, 1L << 3, &ft_close_window, &data);
 	mlx_loop(data.mlx_ptr);
 	ft_lstclear(&(data.lst_map), ft_clear_map);
 	return (0);
--- srcs/parsing/map_to_list.c
+++ srcs/parsing/map_to_list.c
@@ -6,14 +6,14 @@
 /*   By: elouisia <elouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/07 16:04:09 by elouisia          #+#    #+#             */
-/*   Updated: 2022/09/26 10:42:09 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/09/28 15:41:13 by elouisia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 #include <errno.h>
 
-static void	print_lst_map(void *content)
+static void	ft_print_lst_map(void *content)
 {
 	t_map_data	*map_line;
 
@@ -21,7 +21,7 @@ static void	print_lst_map(void *content)
 	ft_putstr_fd(map_line->line, 1);
 }
 
-int	add_line_map(char *line, t_cub_data *data)
+int	ft_add_line_map(char *line, t_cub_data *data)
 {
 	t_map_data	*map_data;
 	t_list		*new;
@@ -36,7 +36,7 @@ int	add_line_map(char *line, t_cub_data *data)
 	return (0);
 }
 
-int	map_to_list(t_cub_data *data, char *map_file)
+int	ft_map_to_list(t_cub_data *data, char *map_file)
 {
 	char		*line;
 	int			fd;
@@ -50,15 +50,15 @@ int	map_to_list(t_cub_data *data, char *map_file)
 	line = get_next_line(fd);
 	if (line == NULL)
 		return (close(fd), ft_err_msg("Empty map."));
-	if (add_line_map(line, data) == WRONG_MALLOC)
+	if (ft_add_line_map(line, data) == WRONG_MALLOC)
 		return (close(fd), ft_err_msg("Malloc error."));
 	while (line)
 	{
 		line = get_next_line(fd);
-		if (line && add_line_map(line, data) == WRONG_MALLOC)
+		if (line && ft_add_line_map(line, data) == WRONG_MALLOC)
 			return (get_next_line(GNL_FLUSH), ft_err_msg("Malloc error."));
 	}
-	ft_lstiter(data->lst_map, &print_lst_map);
+	ft_lstiter(data->lst_map, &ft_print_lst_map);
 	ft_putstr_fd("\n", 1);
 	close(fd);
 	return (0);

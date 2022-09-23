ed1817a0882adb6aee23e9d20fd0f36d71a95d66
Adrian WEAVER <aweaver@student.42.fr>
2022-09-23 09:18:32 +0200

    ADD F AND C COLOUR CHECKS:
    Adding checks for F and C colours.
    
    For now the get_argb function is set as static function in file
    check_ceilling_floor_texture_functions.
    
    This will be changed in later commits when colours will be properly
    handled in the project.

--- srcs/parsing/check_ceilling_floor_texture_functions.c
+++ srcs/parsing/check_ceilling_floor_texture_functions.c
@@ -6,11 +6,45 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/19 11:31:41 by aweaver           #+#    #+#             */
-/*   Updated: 2022/09/23 08:26:12 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/09/23 09:20:20 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
+
+static int	ft_get_argb(int a, int red, int green, int blue)
+{
+	double	argb;
+
+	if (red >= 255)
+		red = 255;
+	if (green >= 255)
+		green = 255;
+	if (blue >= 255)
+		blue = 255;
+	if (red < 0)
+		red = 0;
+	if (green < 0)
+		green = 0;
+	if (blue < 0)
+		blue = 0;
+	argb = a << 24 | red << 16 | green << 8 | blue;
+	return (argb);
+}
+
+int	ft_check_redefinition(t_map_data *map, char *col_name, int *colour,
+	int new_colour)
+{
+	if (*colour == -1)
+	{
+		*colour = new_colour;
+		return (0);
+	}
+	else
+		ft_wrong_map_exit(map->data->lst_map, col_name,
+			"Unauthorized re-definition");
+	return (1);
+}
 
 int	ft_check_value(char *str, int *colour)
 {
@@ -27,6 +61,8 @@ int	ft_check_value(char *str, int *colour)
 		i++;
 	}
 	*colour = ft_atoi(str);
+	if (*colour < 0 || *colour > 255)
+		return (1);
 	return (0);
 }
 
@@ -53,12 +89,35 @@ int	ft_check_floor(t_map_data *map)
 				ft_wrong_map_exit(map->data->lst_map, "Texture F: ",
 					"not a valid colour"), 1);
 	}
+	ft_check_redefinition(map, "F: ", &(map->data->floor),
+		ft_get_argb(0, rgb[0], rgb[1], rgb[2]));
 	return (0);
 }
 
 int	ft_check_ceilling(t_map_data *map)
 {
-	printf("ceilling flag = %s\n", *map->flag_and_path);
-	printf("ceilling path = %s\n", (*(map->flag_and_path + 1)));
+	char	*tmp;
+	int		rgb[3];
+	int		i;
+
+	tmp = ft_strptr_to_str(map->flag_and_path + 1);
+	map->flag_and_path = ft_free_strptr(map->flag_and_path);
+	map->flag_and_path = ft_split(tmp, ',');
+	free(tmp);
+	if (ft_strptr_len(map->flag_and_path) != 3)
+	{
+		ft_free_strptr(map->flag_and_path);
+		ft_wrong_map_exit(map->data->lst_map, "C: ", "not a valid colour");
+	}
+	i = -1;
+	while (++i < 3)
+	{
+		if (ft_check_value(tmp, &rgb[i]) == 1)
+			return (free(tmp), ft_free_strptr(map->flag_and_path),
+				ft_wrong_map_exit(map->data->lst_map, "Texture C: ",
+					"not a valid colour"), 1);
+	}
+	ft_check_redefinition(map, "C: ", &(map->data->floor),
+		ft_get_argb(0, rgb[0], rgb[1], rgb[2]));
 	return (0);
 }

a69f71b6dfd08203ad027f930681f9204b6ad722
Adrian WEAVER <aweaver@student.42.fr>
2022-10-03 14:19:30 +0200

    FIX AFTER REVIEW FOR PLAYER SPAWNS:
    Player spawns are now properly dealt with straight from top scope
    of check_grid without using an unecessary static variable.

--- srcs/parsing/check_map_grid.c
+++ srcs/parsing/check_map_grid.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/03 09:27:26 by aweaver           #+#    #+#             */
-/*   Updated: 2022/10/03 13:55:04 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/10/03 14:19:03 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,10 @@ static int	ft_is_player_starting_position(char c)
 
 static void	ft_is_valid_map_pattern(t_cub_data *data, char **grid, int x, int y)
 {
-	static int	player_position = 0;
-
 	if (grid[y][x] == '1' || grid[y][x] == ' ' || grid[y][x] == '\n')
 		return ;
 	else
 	{
-		if (ft_is_player_starting_position(grid[y][x]))
-			player_position++;
-		if (player_position > 1)
-			ft_exit_check_grid(data, "Redefining player spawn position.\n");
 		if (y == 0 || grid[y + 1] == NULL)
 			ft_exit_check_grid(data, "Map is not closed with walls.\n");
 		if (x == 0 || grid[y][x + 1] == '\n' || grid[y][x + 1] == '\0')
@@ -60,9 +54,9 @@ int	ft_check_map_grid(t_cub_data *data, char **grid)
 	int	x;
 	int	y;
 
-	y = 0;
+	y = -1;
 	player = 0;
-	while (grid[y])
+	while (grid[++y])
 	{
 		x = 0;
 		while (grid[y][x])
@@ -75,9 +69,10 @@ int	ft_check_map_grid(t_cub_data *data, char **grid)
 				return (1);
 			x++;
 		}
-		y++;
 	}
-	if (player == 0)
+	if (player < 1)
 		ft_exit_check_grid(data, "No spawn for player.");
+	if (player > 1)
+		ft_exit_check_grid(data, "Redefining player spawn position.\n");
 	return (0);
 }

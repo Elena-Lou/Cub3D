c4725f05cda9fa60e4e7305e0b0420167424fb32
aweaver <aweaver@student.42.fr>
2022-10-07 14:23:15 +0200

    FIX COMPILE ISSUE CAUSED BY WIP:
    WIP was merged with master this caused a compile issue.
    Commented the whole file to fix it later.

--- srcs/raycasting/dda.c
+++ srcs/raycasting/dda.c
@@ -6,7 +6,7 @@
 /*   By: elouisia <elouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 15:40:38 by elouisia          #+#    #+#             */
-/*   Updated: 2022/10/05 16:55:43 by elouisia         ###   ########.fr       */
+/*   Updated: 2022/10/07 13:26:45 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -53,46 +53,46 @@ A_ Cast a ray
 // 	}
 // }
 
-void	ft_ray_launcher(t_cub_data *data, t_dda *ray, double tan)
-{
-	if (ray->dir_x == 1 && ray->dir_y == -1)
-		ft_launch_no_ea(data, ray, tan);
-	else if (ray->dir_x == -1 && ray->dir_y == -1)
-		ft_launch_no_we(data, ray, tan);
-	else if (ray->dir_x == -1 && ray->dir_y == 1)
-		ft_launch_so_we(data, ray, tan);
-	else
-		ft_launch_no_ea(data, ray, tan);
-}
-
-void	ft_set_dir(t_dda *ray, double tan)
-{
-	if (ray->theta > WEST || ray->theta < EAST)
-		ray->dir_y = -1;
-	else
-		ray->dir_y = 1;
-	if (ray->theta > NORTH || ray->theta < SOUTH)
-		ray->dir_x = 1;
-	else
-		ray->dir_x = -1;
-}
-
-void	ft_set_ray_data(t_cub_data *data)
-{
-	t_dda	ray;
-	double	theta;
-	double	tan_theta;
-
-	ray.pos_x = data->player.x;
-	ray.pos_y = data->player.y;
-	data->player.tile_x = data->player.x >> 6;
-	data->player.tile_y = data->player.y >> 6;
-	ray.theta = data->player.pov - (FOV / 2);
-	tan_theta = tan(theta);
-	ray.inter_x = ray.pos_x + (ray.d_y / tan_theta);
-	ray.inter_y = ray.pos_y + (ray.d_x * tan_theta);
-	ft_set_dir(&ray, tan_theta);
-	ray.step_x = ray.dir_x * tan_theta;
-	ray.step_y = ray.dir_y / tan_theta;
-	ft_ray_launcher(data, &ray, tan_theta);
-}
+//void	ft_ray_launcher(t_cub_data *data, t_dda *ray, double tan)
+//{
+	//if (ray->dir_x == 1 && ray->dir_y == -1)
+		//ft_launch_no_ea(data, ray, tan);
+	//else if (ray->dir_x == -1 && ray->dir_y == -1)
+		//ft_launch_no_we(data, ray, tan);
+	//else if (ray->dir_x == -1 && ray->dir_y == 1)
+		//ft_launch_so_we(data, ray, tan);
+	//else
+		//ft_launch_no_ea(data, ray, tan);
+//}
+//
+//void	ft_set_dir(t_dda *ray, double tan)
+//{
+	//if (ray->theta > WEST || ray->theta < EAST)
+		//ray->dir_y = -1;
+	//else
+		//ray->dir_y = 1;
+	//if (ray->theta > NORTH || ray->theta < SOUTH)
+		//ray->dir_x = 1;
+	//else
+		//ray->dir_x = -1;
+//}
+//
+//void	ft_set_ray_data(t_cub_data *data)
+//{
+	//t_dda	ray;
+	//double	theta;
+	//double	tan_theta;
+//
+	//ray.pos_x = data->player.x;
+	//ray.pos_y = data->player.y;
+	//data->player.tile_x = data->player.x >> 6;
+	//data->player.tile_y = data->player.y >> 6;
+	//ray.theta = data->player.pov - (FOV / 2);
+	//tan_theta = tan(theta);
+	//ray.inter_x = ray.pos_x + (ray.d_y / tan_theta);
+	//ray.inter_y = ray.pos_y + (ray.d_x * tan_theta);
+	//ft_set_dir(&ray, tan_theta);
+	//ray.step_x = ray.dir_x * tan_theta;
+	//ray.step_y = ray.dir_y / tan_theta;
+	//ft_ray_launcher(data, &ray, tan_theta);
+//}

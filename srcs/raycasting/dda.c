5a3ef4d56ebf85981b5e9629296f0f4f90744b8d
Elena-Lou <elouisiade@live.fr>
2022-10-19 12:02:27 +0200

    UPDATE FILES TO REMOVE DEBUG ASSISTANTS:
    The printf are removed to recover clean working files.

--- srcs/raycasting/dda.c
+++ srcs/raycasting/dda.c
@@ -6,7 +6,7 @@
 /*   By: elouisia <elouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/02 15:40:38 by elouisia          #+#    #+#             */
-/*   Updated: 2022/10/20 15:02:25 by elouisia         ###   ########.fr       */
+/*   Updated: 2022/10/20 15:05:59 by elouisia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,39 +24,21 @@ void	ft_set_dir(t_dda *ray)
 		ray->facing_left = TRUE;
 }
 
-// double	ft_normalize_angle(double theta)
-// {
-// 	if (theta < 0)
-// 		theta += (2 * PI);
-// 	return (theta);
-// }
-
 void	ft_set_ray_data(t_cub_data *data)
 {
 	t_dda	ray[WIDTH];
 	int		i;
 
 	i = 0;
-	// printf("\n ----SET RAY DATA----\n");
 	ft_distance_to_projection_plane(&data->player);
-	// printf("set ray --- after distpp\n");
 	ray[0].theta = data->player.pov - (FOV / 2);
-	// printf("ray[0] theta : %f\n", ray[i].theta);
 	while (i < WIDTH)
 	{
-		// printf("while set ray\n");
-		// printf("ray[%d] theta : %f\n", i,  ray[i].theta);
 		ray[i].id = i;
 		ft_cast_ray(data, &ray[i]);
-		// printf("after ft_cast_ray\n");
 		ft_wall_projection(data, &ray[i]);
-		// printf("after wall projection\n");
 		if (i + 1 < WIDTH)
 			ray[i + 1].theta = ray[i].theta	+ (FOV / WIDTH);
 		i++;
-
-		// ray[i].theta = (ray[i].theta + (FOV / WIDTH));
-		// printf("ray[%d] theta : %f\n", i,  ray[i].theta);
-
 	}
 }

003e28cfcbf049abce9c71faf517460cb6406e6f
aweaver <aweaver@student.42.fr>
2022-10-20 07:13:03 +0200

    FIX PLAYER ROTATION WHILE MOVING:
    Trying to fix player rotation when moving, this is still not ideal.
    The mlx seems to be the main issue here, not being able to time frames.

--- srcs/movement/player_rotation.c
+++ srcs/movement/player_rotation.c
@@ -6,7 +6,7 @@
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/07 17:41:24 by aweaver           #+#    #+#             */
-/*   Updated: 2022/10/17 17:44:35 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/10/20 05:57:01 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ int	ft_rotate_left(t_cub_data *data)
 	data->player.pov -= ((double)PI / 180.0);
 	if (data->player.pov < 0.0)
 		data->player.pov = (double)PI * 2.0 + data->player.pov;
-	return (0);
+	return (1);
 }
 
 int	ft_rotate_right(t_cub_data *data)
@@ -25,5 +25,5 @@ int	ft_rotate_right(t_cub_data *data)
 	data->player.pov += ((double)PI / 180.0);
 	if (data->player.pov > ((double)PI * 2.0))
 		data->player.pov = ((double)PI * 2.0) - data->player.pov;
-	return (0);
+	return (1);
 }

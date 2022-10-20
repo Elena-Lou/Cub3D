92383669371c0304214eb5478e5f7566e3d6e9f2
Adrian WEAVER <aweaver@student.42.fr>
2022-10-20 14:46:18 +0200

    UPDATE NORM:
    Updating the norm splitting ft_move in two functions ft_move now
    calls ft_rotate.

--- srcs/movement/key_checks.c
+++ srcs/movement/key_checks.c
@@ -6,25 +6,17 @@
 /*   By: elouisia <elouisia@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/09/28 16:32:30 by elouisia          #+#    #+#             */
-/*   Updated: 2022/10/20 07:11:25 by aweaver          ###   ########.fr       */
+/*   Updated: 2022/10/20 14:14:21 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	ft_move(t_cub_data *data)
+static int	ft_rotate(t_cub_data *data)
 {
 	int	moved;
 
 	moved = 0;
-	if (data->player.move_y == 1)
-		moved += ft_move_forward(data);
-	if (data->player.move_y == -1)
-		moved += ft_move_backward(data);
-	if (data->player.move_x == -1)
-		moved += ft_strafe_left(data);
-	if (data->player.move_x == 1)
-		moved += ft_strafe_right(data);
 	if (data->player.rotate < 0)
 	{
 		data->player.rotate--;
@@ -41,6 +33,23 @@ int	ft_move(t_cub_data *data)
 		else if (moved)
 			moved += ft_rotate_left(data);
 	}
+	return (moved);
+}
+
+int	ft_move(t_cub_data *data)
+{
+	int	moved;
+
+	moved = 0;
+	if (data->player.move_y == 1)
+		moved += ft_move_forward(data);
+	if (data->player.move_y == -1)
+		moved += ft_move_backward(data);
+	if (data->player.move_x == -1)
+		moved += ft_strafe_left(data);
+	if (data->player.move_x == 1)
+		moved += ft_strafe_right(data);
+	moved += ft_rotate(data);
 	return (moved);
 }
 

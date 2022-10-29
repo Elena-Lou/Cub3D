063c626eccd58d74af18fa611e143000faa749cd
Adrian WEAVER <aweaver@student.42.fr>
2022-10-29 16:24:50 +0200

    UPDATE BONUS FILES:
    Updating all bonus files header and content.

--- srcs/textures/pick_colours_bonus.c
+++ srcs/textures/pick_colours_bonus.c
@@ -5,23 +5,20 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: aweaver <aweaver@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/29 16:02:08 by aweaver           #+#    #+#             */
-/*   Updated: 2022/10/29 16:03:33 by aweaver          ###   ########.fr       */
+/*   Created: 2022/10/29 16:29:46 by aweaver           #+#    #+#             */
+/*   Updated: 2022/10/29 16:29:53 by aweaver          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-int	ft_pick_colour(t_cub_data *data, t_dda *ray, int y_offset, int x_offset)
+int	ft_pick_colour(t_cub_data *data, t_dda *ray, int y, int x_offset)
 {
+	int	y_offset;
 	int	colour;
 	int	index;
 	int	wall_orientation;
 
-	if (x_offset < 0)
-		x_offset = 0;
-	if (y_offset < 0)
-		y_offset = 0;
 	if (ray->hzt_hit == TRUE && ray->facing_up == TRUE)
 		wall_orientation = NO;
 	else if (ray->hzt_hit == TRUE && ray->facing_up == FALSE)
@@ -30,12 +27,14 @@ int	ft_pick_colour(t_cub_data *data, t_dda *ray, int y_offset, int x_offset)
 		wall_orientation = EA;
 	else
 		wall_orientation = WE;
+	y_offset = ft_texture_y_offset(ray, y, data->tex[wall_orientation].height);
+	x_offset = x_offset * data->tex[wall_orientation].width / 64;
 	if (x_offset >= data->tex[wall_orientation].width)
 		x_offset = data->tex[wall_orientation].width - 1;
 	if (y_offset >= data->tex[wall_orientation].height)
 		y_offset = data->tex[wall_orientation].height - 1;
-	index = (data->tex[wall_orientation].line_len * y_offset)
-		+ (x_offset * data->tex[wall_orientation].bpp / 8);
+	index = ((data->tex[wall_orientation].line_len * y_offset)
+			+ (x_offset * data->tex[wall_orientation].bpp / 8));
 	colour = ((int *)data->tex[wall_orientation].addr)[index / 4];
 	return (colour);
 }
